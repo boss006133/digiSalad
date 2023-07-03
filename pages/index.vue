7bc9bf4b4af2b31216c2946edcd069b7dd4a7028
DESKTOP-9GH44K3\User <boss00613333@gmail.com>
2023-07-04 02:35:48 +0800

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -47,7 +47,7 @@
         </div>
         <div :id="`${aboutVideoId}`" class="about-video"></div>
       </div>
-      <div class="content">
+      <div class="sec-content">
         <p>
           Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras
           ullamcorper bibendum bibendum. Duis tincidunt urna non pretium porta.
@@ -71,9 +71,29 @@
     <!-- AWARDS -->
     <section class="awards">
       <div class="awards-board-wrap">
-        <div class="awards-board"></div>
+        <div class="awards-board">
+          <div class="contentBox">
+            <div class="sec-title-warp">
+              <div class="sec-title">
+                <div class="text">AWARDS<span class="dot"></span></div>
+                <div class="wave"></div>
+              </div>
+            </div>
+            <div class="sec-content">
+              Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras
+              ullamcorper bibendum bibendum. Duis tincidunt urna non pretium
+              porta. Nam condimentum vitae ligula vel ornare. Phasellus at
+              semper turpis. Nunc eu tellus tortor. Etiam at condimentum nisl,
+              vitae sagittis orci. Donec id dignissim nunc.
+            </div>
+          </div>
+          <div class="awardList">
+            <div class="item" v-for="(item, index) in 4" :key="index">
+              <img src="/image/award-1.png" width="283" height="108" />
+            </div>
+          </div>
+        </div>
       </div>
-
       <div class="half-bg"></div>
     </section>
     <!-- OUR INGRADIENTS -->
@@ -82,6 +102,26 @@
         <div class="sec-title">
           <div class="text">OUR INGRADIENTS<span class="dot"></span></div>
           <div class="wave gray"></div>
+        </div>
+      </div>
+      <div class="sec-content">
+        <p>
+          Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras
+          ullamcorper bibendum bibendum. Duis tincidunt urna non pretium porta.
+          Nam condimentum vitae ligula vel ornare. Phasellus at semper turpis.
+          Nunc eu tellus tortor. Etiam at condimentum nisl, vitae sagittis orci.
+          Donec id dignissim nunc. Donec elit ante, eleifend a dolor et,
+          venenatis facilisis dolor. In feugiat orci odio, sed lacinia sem
+          elementum quis. Aliquam consectetur, eros et vulputate euismod, nunc
+          leo tempor lacus, ac rhoncus neque eros nec lacus. Cras lobortis
+          molestie faucibus.
+        </p>
+      </div>
+      <div class="btn__container">
+        <div class="item">
+          <button class="ds-button ds-button--second">
+            <span class="text">OUR SERVICES</span>
+          </button>
         </div>
       </div>
     </section>
