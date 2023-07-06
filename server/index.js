f2a179fad76980c25e70dc1dcb00e6e3f88bfca4
DESKTOP-9GH44K3\User <boss00613333@gmail.com>
2023-07-06 11:31:02 +0800

    no message

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ async function start() {
   // Listen the server
   const server_http = http.createServer(app)
 
-  server_http.listen(port, host, () => {
+  server_http.listen(port || 5000, host, () => {
     consola.ready({
       message: `Server listening on http://${host}:${port}`,
       badge: true,
